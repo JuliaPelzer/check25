50c055fea3695fa02a1dab44a85923bb661e7f07
Christopher Wróbel <wrobel1999@gmail.com>
2023-11-19 00:39:12 +0100

    Add show more

--- app/static/results.js
+++ app/static/results.js
@@ -1,23 +1,36 @@
+let displayed = 0;
+let craftsmen;
+
 async function make_interactive() {
   let postalcode = new URLSearchParams(window.location.search).get("postalcode");
 
   let response = await fetch("/craftsmen?postalcode=" + postalcode);
 
   let data = await response.json();
-  let craftsmen = data["craftsmen"];
-
-  let provider_list = document.getElementById("provider_list");
-  const template = document.querySelector("#provider");
+  craftsmen = data["craftsmen"];
 
   let numba = document.getElementById("numba");
   numba.textContent = "Yeahy, we found " + craftsmen.length + " craftmen in your area!"
+  
+  if (craftsmen.length < 20) {
+    document.getElementById("more").remove()
+  }
+  
+  show_more()
+}
 
-  for (let i = 0; i < craftsmen.length && i < 20; i++) {
+function show_more() {
+  let provider_list = document.getElementById("provider_list");
+  const template = document.querySelector("#provider");
+  
+  let limit = displayed + 20;
+
+  for (let i = displayed; i < craftsmen.length && i < limit; i++) {
     const clone = template.content.cloneNode(true);
     let name = clone.querySelector("h3");
-    console.log(name)
     name.textContent = craftsmen[i]["name"];
     provider_list.appendChild(clone);
+    displayed = i + 1;
   }
 }
 
