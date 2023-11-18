45b9b8959944be98dd053db4c9b1d18a7d8bfd34
Christopher Wróbel <wrobel1999@gmail.com>
2023-11-18 22:53:26 +0100

    Add data from api to website

---
+++ app/static/results.js
@@ -0,0 +1,21 @@
+async function make_interactive() {
+  let postalcode = new URLSearchParams(window.location.search).get("postalcode");
+
+  let response = await fetch("/craftsmen?postalcode=" + postalcode);
+  
+  let data = await response.json();
+  let craftsmen = data["craftsmen"];
+
+  let provider_list = document.getElementById("provider_list");
+  const template = document.querySelector("#provider");
+  
+  for (let i = 0; i < craftsmen.length; i++) {
+    const clone = template.content.cloneNode(true);
+    let name = clone.querySelector("h3");
+    console.log(name)
+    name.textContent = craftsmen[i]["name"];
+    provider_list.appendChild(clone);
+  }
+}
+
+make_interactive()
